b38e8d246b05057db0cd98a6426c22f3d4691b28
Mike Paulonis <55862757+mpaulonis@users.noreply.github.com>
2024-05-08 10:59:55 -0400

    Added DriveToPose command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
+import static java.lang.Math.PI;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -25,13 +30,56 @@ public final class Constants {
   public static final Mode currentMode = Mode.REAL;
 
   public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
+    REAL, // running on a real robot
+    SIM,  // running on a physics simulator
+    REPLAY // replaying from a log file
+  }
 
-    /** Running a physics simulator. */
-    SIM,
+  public static class DriveConstants {
+    public static final double MAX_LINEAR_SPEED = Units.feetToMeters(16.6); // meters per second
+    public static final double TRACK_WIDTH_X =
+        Units.inchesToMeters(24.75); // meters - 5.25 inches less than frame width for MK4i
+    public static final double TRACK_WIDTH_Y =
+        Units.inchesToMeters(23.75); // meters - 5.25 inches less than frame length for MK4i
+    public static final double DRIVE_BASE_RADIUS =
+        Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0); // meters
+    public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS; // radians per second
 
-    /** Replaying from a log file. */
-    REPLAY
+    private DriveConstants() {
+
+    }
+  }
+
+  public static class AutoConstants {
+    
+    public static final double X_kP = 5.0;
+    public static final double X_kI = 0.0;
+    public static final double X_kD = 0.0;
+
+    public static final double Y_kP = 5.0;
+    public static final double Y_kI = 0.0;
+    public static final double Y_kD = 0.0;
+
+    public static final TrapezoidProfile.Constraints THETA_CONSTRAINTS = new TrapezoidProfile.Constraints(PI, 2 / PI);
+    public static final double THETA_kP = 6.0;
+    public static final double THETA_kI = 0.02;
+    public static final double THETA_kD = 0.0;
+
+    public static final double PATH_THETA_kP = 2.6;
+    public static final double PATH_THETA_kI = 0.001;
+    public static final double PATH_THETA_kD = 0.0;
+
+    private AutoConstants() {
+
+    }
+  }
+
+  public static class VisionConstants {
+    public static final double FIELD_LENGTH = Units.inchesToMeters(651.223); // length in meters
+    public static final double FIELD_WIDTH = Units.inchesToMeters(323.277); // length in meters
+
+    private VisionConstants() {
+
+    }
   }
 }
---
+++ src/main/java/frc/robot/commands/DriveToPoseCommand.java
@@ -0,0 +1,140 @@
+package frc.robot.commands;
+
+import static frc.robot.Constants.AutoConstants.THETA_kD;
+import static frc.robot.Constants.AutoConstants.THETA_kI;
+import static frc.robot.Constants.AutoConstants.THETA_kP;
+import static frc.robot.Constants.AutoConstants.X_kD;
+import static frc.robot.Constants.AutoConstants.X_kI;
+import static frc.robot.Constants.AutoConstants.X_kP;
+import static frc.robot.Constants.AutoConstants.Y_kD;
+import static frc.robot.Constants.AutoConstants.Y_kI;
+import static frc.robot.Constants.AutoConstants.Y_kP;
+import static frc.robot.Constants.DriveConstants.MAX_LINEAR_SPEED;
+import static frc.robot.Constants.DriveConstants.MAX_ANGULAR_SPEED;
+import static frc.robot.Constants.VisionConstants.FIELD_WIDTH;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.subsystems.drive.Drive;
+
+public class DriveToPoseCommand {
+
+  private static final double TRANSLATION_TOLERANCE = 0.02;
+  private static final double THETA_TOLERANCE = Units.degreesToRadians(2.0);
+
+  /** Default constraints are 90% of max speed, accelerate to full speed in 1/3 second */
+  private static final TrapezoidProfile.Constraints DEFAULT_XY_CONSTRAINTS = new TrapezoidProfile.Constraints(
+      MAX_LINEAR_SPEED * 0.5,
+      MAX_LINEAR_SPEED);
+  private static final TrapezoidProfile.Constraints DEFAULT_OMEGA_CONSTRAINTS = new TrapezoidProfile.Constraints(
+      MAX_ANGULAR_SPEED * 0.4,
+      MAX_ANGULAR_SPEED);
+
+  private final ProfiledPIDController xController;
+  private final ProfiledPIDController yController;
+  private final ProfiledPIDController thetaController;
+
+  private final Drive drive;
+  private final Supplier<Pose2d> poseProvider;
+  private final Pose2d goalPose;
+  private final boolean useAllianceColor;
+
+  public DriveToPoseCommand(
+        Drive drive,
+        Supplier<Pose2d> poseProvider,
+        Pose2d goalPose,
+        boolean useAllianceColor) {
+    this(drive, poseProvider, goalPose, DEFAULT_XY_CONSTRAINTS, DEFAULT_OMEGA_CONSTRAINTS, useAllianceColor);
+  }
+
+  public DriveToPoseCommand(
+        Drive drive,
+        Supplier<Pose2d> poseProvider,
+        Pose2d goalPose,
+        TrapezoidProfile.Constraints xyConstraints,
+        TrapezoidProfile.Constraints omegaConstraints,
+        boolean useAllianceColor) {
+    this.drive = drive;
+    this.poseProvider = poseProvider;
+    this.goalPose = goalPose;
+    this.useAllianceColor = useAllianceColor;
+
+    xController = new ProfiledPIDController(X_kP, X_kI, X_kD, xyConstraints);
+    yController = new ProfiledPIDController(Y_kP, Y_kI, Y_kD, xyConstraints);
+    xController.setTolerance(TRANSLATION_TOLERANCE);
+    yController.setTolerance(TRANSLATION_TOLERANCE);
+    thetaController = new ProfiledPIDController(THETA_kP, THETA_kI, THETA_kD, omegaConstraints);
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    thetaController.setTolerance(THETA_TOLERANCE);
+  }
+
+  public void initialize() {
+    resetPIDControllers();
+    var pose = goalPose;
+    boolean allianceRed = false;
+    Optional<Alliance> ally = DriverStation.getAlliance();
+    if (ally.isPresent()) {
+        if (ally.get() == Alliance.Red) {
+            allianceRed = true;
+        }
+    }
+    if (useAllianceColor && allianceRed) {
+      Translation2d transformedTranslation = new Translation2d(pose.getX(), FIELD_WIDTH - pose.getY());
+      Rotation2d transformedHeading = pose.getRotation().times(-1);
+      pose = new Pose2d(transformedTranslation, transformedHeading);
+    }
+    thetaController.setGoal(pose.getRotation().getRadians());
+    xController.setGoal(pose.getX());
+    yController.setGoal(pose.getY());  }
+
+  public boolean atGoal() {
+    return xController.atGoal() && yController.atGoal() && thetaController.atGoal();
+  }
+
+  private void resetPIDControllers() {
+    var robotPose = poseProvider.get();
+    thetaController.reset(robotPose.getRotation().getRadians());
+    xController.reset(robotPose.getX());
+    yController.reset(robotPose.getY());
+  }
+
+  public void execute() {
+    var robotPose = poseProvider.get();
+    // Drive to the goal
+    var xSpeed = xController.calculate(robotPose.getX());
+    if (xController.atGoal()) {
+      xSpeed = 0;
+    }
+
+    var ySpeed = yController.calculate(robotPose.getY());
+    if (yController.atGoal()) {
+      ySpeed = 0;
+    }
+
+    var omegaSpeed = thetaController.calculate(robotPose.getRotation().getRadians());
+    if (thetaController.atGoal()) {
+      omegaSpeed = 0;
+    }
+
+    drive.runVelocity(
+      ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, omegaSpeed, robotPose.getRotation()));
+  }
+
+  public boolean isFinished() {
+    return atGoal();
+  }
+
+  public void end(boolean interrupted) {
+    drive.stop();
+  }
+}
