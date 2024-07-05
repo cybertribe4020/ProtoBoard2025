3075a1aab5cb8e062bc2faa2d634dee4969c5112
Mike Paulonis <55862757+mpaulonis@users.noreply.github.com>
2024-07-05 12:34:56 -0400

    Testing 3 and 5 note autos on the robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,7 +35,7 @@ import edu.wpi.first.math.util.Units;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final Mode currentMode = Mode.SIM;
+  public static final Mode currentMode = Mode.REAL;
   public static final Integer NUM_CAMS = 2;
 
   public static enum Mode {
